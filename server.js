913f1d4444d6b5f4d34d9cce99f6d12ad957c675
Chamuth Chamandana <c.chamandana@gmail.com>
2021-04-18 20:01:29 +0530

    removed post req

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
 const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
 const Router = require('koa-router');
 const { receiveWebhook, registerWebhook } = require('@shopify/koa-shopify-webhooks');
-const bodyParser = require('koa-bodyparser');
 
 const { Pool } = require("pg");
 
@@ -28,7 +27,7 @@ const {
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
-  server.use(bodyParser);
+  
   server.use(session({ sameSite: 'none', secure: true }, server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
 
@@ -66,10 +65,6 @@ app.prepare().then(() => {
   router.get("/api/rates", async (ctx) => {
     const { rows } = await ctx.app.pool.query("SELECT * FROM exchange_rates")
     ctx.body = JSON.stringify(rows);
-  })
-
-  router.post("/api/rates", async (ctx) => {
-    ctx.body = JSON.stringify(ctx.request.body);
   })
 
   server.use(graphQLProxy({ version: ApiVersion.July20 }));
