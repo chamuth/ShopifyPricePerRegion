0091071929b0f2357b7e1fdbade55f8f4073aa34
Chamuth Chamandana <c.chamandana@gmail.com>
2021-04-22 11:06:43 +0530

    log gql error

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -135,7 +135,8 @@ const ProductCard = (props) =>
     }
     console.log(JSON.stringify(input));
     updateProduct({ variables: { input : input } })
-      .then(() => {
+      .then((er) => {
+        console.log(JSON.stringify(er));
         props.refetch();
       });
 
