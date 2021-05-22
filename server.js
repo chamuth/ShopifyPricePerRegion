1779ae3da6f108f8d5d12d09d6ab6039d9e08d57
Chamuth Chamandana <c.chamandana@gmail.com>
2021-05-22 23:57:43 +0530

    Order create webhook handle

--- server.js
+++ server.js
@@ -1,94 +1,100 @@
-require('isomorphic-fetch');
-const dotenv = require('dotenv');
-dotenv.config();
-const Koa = require('koa');
-const next = require('next');
-const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
-const { verifyRequest } = require('@shopify/koa-shopify-auth');
-const session = require('koa-session');
-const { default: graphQLProxy } = require('@shopify/koa-shopify-graphql-proxy');
-const { ApiVersion } = require('@shopify/koa-shopify-graphql-proxy');
-const Router = require('koa-router');
-const { receiveWebhook, registerWebhook } = require('@shopify/koa-shopify-webhooks');
-
-const { Pool } = require("pg");
-
-const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
+require("isomorphic-fetch")
+const dotenv = require("dotenv")
+dotenv.config()
+const Koa = require("koa")
+const next = require("next")
+const { default: createShopifyAuth } = require("@shopify/koa-shopify-auth")
+const { verifyRequest } = require("@shopify/koa-shopify-auth")
+const session = require("koa-session")
+const { default: graphQLProxy } = require("@shopify/koa-shopify-graphql-proxy")
+const { ApiVersion } = require("@shopify/koa-shopify-graphql-proxy")
+const Router = require("koa-router")
 const {
-  SHOPIFY_API_SECRET_KEY,
-  SHOPIFY_API_KEY,
-  HOST,
-} = process.env;
-  
-app.prepare().then(() => {
-  const server = new Koa();
-  const router = new Router();
+  receiveWebhook,
+  registerWebhook,
+} = require("@shopify/koa-shopify-webhooks")
 
-  server.use(session({ sameSite: 'none', secure: true }, server));
-  server.keys = [SHOPIFY_API_SECRET_KEY];
+const { Pool } = require("pg")
+
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== "production"
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, HOST } = process.env
+
+app.prepare().then(() => {
+  const server = new Koa()
+  const router = new Router()
+
+  server.use(session({ sameSite: "none", secure: true }, server))
+  server.keys = [SHOPIFY_API_SECRET_KEY]
 
   server.pool = new Pool({
-    user: 'prrsbxxhpdgjwi',
-    host: 'ec2-52-23-45-36.compute-1.amazonaws.com',
-    database: 'd3nb7unse3sq4u',
-    password: 'b470a762490ee281efa562d75def798a019c3b4dac411bb2737f2eeb4cc77965', 
+    user: "prrsbxxhpdgjwi",
+    host: "ec2-52-23-45-36.compute-1.amazonaws.com",
+    database: "d3nb7unse3sq4u",
+    password:
+      "b470a762490ee281efa562d75def798a019c3b4dac411bb2737f2eeb4cc77965",
     port: 5432,
-    ssl: { rejectUnauthorized: false }
-  });
+    ssl: { rejectUnauthorized: false },
+  })
 
   server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
-      scopes: ['read_products', 'write_products'],
+      scopes: ["read_products", "write_products"],
       async afterAuth(ctx) {
-        const { shop, accessToken } = ctx.session;
+        const { shop, accessToken } = ctx.session
         ctx.cookies.set("shopOrigin", shop, {
           httpOnly: false,
           secure: true,
-          sameSite: 'none'
-        });
-      }
+          sameSite: "none",
+        })
+      },
     })
-  );
+  )
 
-  const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY });
+  const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY })
 
-  router.post('/webhooks/products/create', webhook, (ctx) => {
-    console.log('received webhook: ', ctx.state.webhook);
-  });
+  router.post("/webhooks/order/create", webhook, (ctx) => {
+    console.log("received webhook: ", ctx.state.webhook)
+  })
 
   router.get("/api/rates", async (ctx) => {
     const { rows } = await ctx.app.pool.query("SELECT * FROM exchange_rates")
-    ctx.body = JSON.stringify(rows);
+    ctx.body = JSON.stringify(rows)
   })
 
   router.post("/api/rates", (ctx) => {
-    var eurusd = ctx.request.query["EURUSD"];
-    var eurgbp = ctx.request.query["EURGBP"];
-    
+    var eurusd = ctx.request.query["EURUSD"]
+    var eurgbp = ctx.request.query["EURGBP"]
+
     // ctx.body = "UPDATE exchange_rates SET value=" + usdeur + " WHERE key='USDEUR'";
 
-    ctx.app.pool.query(`UPDATE exchange_rates SET value=` + eurusd + ` WHERE key='EURUSD'`);
-    ctx.app.pool.query(`UPDATE exchange_rates SET value=` + eurgbp + ` WHERE key='EURGBP'`);
+    ctx.app.pool.query(
+      `UPDATE exchange_rates SET value=` + eurusd + ` WHERE key='EURUSD'`
+    )
+    ctx.app.pool.query(
+      `UPDATE exchange_rates SET value=` + eurgbp + ` WHERE key='EURGBP'`
+    )
   })
 
-  server.use(graphQLProxy({ version: ApiVersion.July20 }));
+  router.post("/api/ordercreatehook")
 
-  router.get('(.*)', verifyRequest(), async (ctx) => {
-    await handle(ctx.req, ctx.res);
-    ctx.respond = false;
-    ctx.res.statusCode = 200;
-  });
+  server.use(graphQLProxy({ version: ApiVersion.July20 }))
 
-  server.use(router.allowedMethods());
-  server.use(router.routes());
+  router.get("(.*)", verifyRequest(), async (ctx) => {
+    await handle(ctx.req, ctx.res)
+    ctx.respond = false
+    ctx.res.statusCode = 200
+  })
+
+  server.use(router.allowedMethods())
+  server.use(router.routes())
 
   server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`);
-  });
-});
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
